7fcfdceed14d9a3558133256f72540ffbe170a05
James <contact@james-wright.co.uk>
2020-01-26 16:17:23 +0000

    adds recipes saga

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,9 +1,16 @@
-const INITIAL_STATE = {
-  data: "foo",
-};
+import { FETCH_RECIPES_SUCCESS } from '../constants/index';
+const initalState = null;
 
-export const recipeReducer = (state = INITIAL_STATE, action) => {
+export const recipeReducer = (state = initalState, action) => {
+
+
   switch (action.type) {
+    case FETCH_RECIPES_SUCCESS:
+      const data = action.payload;
+      return {
+        ...data
+      }
+
     default:
       return state
   }
--- redux/sagas/sagas.js
+++ redux/sagas/sagas.js
@@ -2,10 +2,25 @@
 import {
   FETCH_RECIPES
 } from '../constants/index'
-import { takeLatest } from 'redux-saga/effects'
+import { fetchRecipesSuccess } from '../actions/index'
+import { takeLatest, put, call } from 'redux-saga/effects'
+import * as firebase from 'firebase';
 
 const fetchReceipes = function* () {
-  console.log('fetch');
+
+  // firebase.database().ref('/recipes/')
+  // .once('value')
+  // .then(function(snapshot) {
+  //   return snapshot.val();
+  // })
+  // .catch(function(error){
+  //   console.log('qwe');
+  // })
+  const response = yield firebase.database().ref("/recipes/").once('value').then(function(snapshot){
+    return snapshot.val();
+  });
+
+  yield put( fetchRecipesSuccess(response) );
 }
 
 export function* recipesWatcher() {
