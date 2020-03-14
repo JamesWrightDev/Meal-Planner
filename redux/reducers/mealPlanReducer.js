5c300a443063a8fefe3a0112b3b6cf5568a67f6d
James <contact@james-wright.co.uk>
2020-03-14 15:50:42 +0000

    Fixes meal plan adding and icnrementing

--- redux/reducers/mealPlanReducer.js
+++ redux/reducers/mealPlanReducer.js
@@ -1,23 +1,47 @@
 import { ADD_RECIPE_MEAL_PLAN, CREATE_SHOPPING_LIST } from "../constants/index";
 
 const initalState = {
-  recipes: [],
+  recipes: [
+  ],
   ingredients: [],
   shoppingList: []
 };
 
 const list = [];
 
+const updateMealplan = (state, action) => {
+  const itemExisits = state.recipes.filter(
+    item => item.id === action.payload.id
+  );
+
+  if (itemExisits.length === 0) {
+    return [...state.recipes, { id: action.payload.id, quantity: 1 }];
+  }
+
+  return state.recipes.map(item => {
+    if (item.id !== action.payload.id) {
+      return item;
+    }
+
+    let { quantity } = item;
+    quantity += 1;
+
+    return {
+      id: item.id,
+      quantity
+    };
+  });
+};
+
 const mealPlanReducer = (state = initalState, action) => {
   switch (action.type) {
     case ADD_RECIPE_MEAL_PLAN:
       return {
         ...state,
-        recipes: [...state.recipes, {name: action.payload.name, id: action.payload.id}],
-        ingredients: [...state.ingredients, ...action.payload.ingredients]
+        recipes: updateMealplan(state, action)
       };
-    case CREATE_SHOPPING_LIST:
 
+    case CREATE_SHOPPING_LIST:
       state.ingredients.forEach(item => {
         const duplicate = list.find(element => element.id === item.id);
         if (duplicate) {
