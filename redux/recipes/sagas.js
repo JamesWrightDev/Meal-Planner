14999f11de2aea302c4271b16948d34c0dbe18fb
James <contact@james-wright.co.uk>
2020-03-20 11:56:36 +0000

    Adds fail case for recipes api fetch

--- redux/recipes/sagas.js
+++ redux/recipes/sagas.js
@@ -1,14 +1,17 @@
 import { takeLatest, put } from "redux-saga/effects";
 import { FETCH_RECIPES } from "../constants/index";
-import { fetchRecipesSuccess } from "./actions";
+import { fetchRecipesSuccess, fetchRecipesFail } from "./actions";
 
 const fetchReceipes = function*() {
-  const baseUrl = "http://192.168.0.105:1337";
-
-  const recipes = yield fetch(`${baseUrl}/recipes`).then(response =>
-    response.json()
-  );
-  yield put(fetchRecipesSuccess(recipes));
+  const baseUrl = "http://192.168.0.106:1337";
+  try {
+    const recipes = yield fetch(`${baseUrl}/recipes`).then(response =>
+      response.json()
+    );
+    yield put(fetchRecipesSuccess(recipes));
+  }catch(error){
+    yield put(fetchRecipesFail(error));
+  }
 };
 
 export default function* recipesWatcher() {
